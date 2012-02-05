302442e114592892c351cbd2ed4ea60fca82e2b8
Phill Sparks <me@phills.me.uk>
2012-02-05 15:23:38 +0000

    Exclude migrations

--- config/default.php
+++ config/default.php
@@ -4,6 +4,11 @@ return array(
 	'destination'  => 'api',
 	'update-check' => 'no',
 	'exclude'      => array(
+		// The stub is not "real" PHP and breaks the ApiGen parser
 		'*/laravel/cli/tasks/migrate/stub.php',
+
+		// Migration class names do not have to be unique, but this causes
+		// problems with the ApiGen parser
+		'*/migrations/*',
 	),
 );
